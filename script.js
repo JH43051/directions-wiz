c76b5bfaf669de9addb84a435ab38f2d5552838a
John Holler <jh43051@gmail.com>
2019-05-21 18:31:47 -0400

    structure, style, and basic js mostly complete

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 // Wrapper function
-window.onload = () => {
+window.onload = function() {
+
+browserGreeting();
+tabListener();
+
+// Tests for user's browser and displays custom greeting
+function browserGreeting() {
+	var browserText = document.getElementById("browserGreet");
+	var greetText = "Get Free Maps and Driving Directions for";
+
+	if (/firefox/i.test(navigator.userAgent) === true) {
+		browserText.innerHTML = greetText + " FireFox!";
+	} else if (/trident/i.test(navigator.userAgent) === true ||
+							/msie/i.test(navigator.userAgent) === true) {
+		browserText.innerHTML = greetText + " Internet Explorer!";
+	} else if (/edge/i.test(navigator.userAgent) === true) {
+		browserText.innerHTML = greetText + " Edge!";
+	} else if (/chrome/i.test(navigator.userAgent) === true) {
+		browserText.innerHTML = greetText + " Chrome!";
+	} else {
+		browserText.innerHTML = "Get Free Maps and Driving Directions!";
+	}
+}
+
+function tabListener() {
+	var tabMap = document.getElementById("tabMap");
+	var tabDirections = document.getElementById("tabDirections");
+
+	tabMap.onclick = tabMapSwitcher;
+	tabDirections.onclick = tabDirectionsSwitcher;
+}
+
+function tabMapSwitcher() {
+	var tabMapClass = tabMap.getAttributeNode("class").value;
+
+	if (tabMapClass === "inactive") {
+		tabMap.getAttributeNode("class").value = "active";
+		tabDirections.getAttributeNode("class").value = "inactive";
+	}
+}
+
+function tabDirectionsSwitcher() {
+	var tabDirClass = tabDirections.getAttributeNode("class").value;
+
+	if (tabDirClass === "inactive") {
+		tabDirections.getAttributeNode("class").value = "active";
+		tabMap.getAttributeNode("class").value = "inactive";
+	}
+}
 
 
-};
+
+
+
+}; // window.onload
