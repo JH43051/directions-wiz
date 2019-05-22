82dc0d6cb59e8b9526bcc5f2273e1cb585a868f0
John Holler <jh43051@gmail.com>
2019-05-22 19:30:50 -0400

    map popups working

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // Wrapper function
 window.onload = function() {
 
+/////////////////////////////////////////////////////////////////////
+// Global variables
+/////////////////////////////////////////////////////////////////////
+
+var tabMap = document.getElementById("tabMap");
+var tabDirections = document.getElementById("tabDirections");
+var formMap = document.getElementById("formMap");
+var popUpBackground = document.getElementById("popUpBackground");
+var searchingPopup = document.getElementById("searchingPopup");
+var directionsPopup = document.getElementById("directionsPopup");
+var mapPopup = document.getElementById("mapPopup");
+
+////////////////////////////////////////////////////////////////////
+// Procedural Function Calls
+////////////////////////////////////////////////////////////////////
+
 browserGreeting();
 tabListener();
+mainButtonListener();
+popupCloseListener();
+
+////////////////////////////////////////////////////////////////////
+// Functions
+////////////////////////////////////////////////////////////////////
 
 // Tests for user's browser and displays custom greeting
 function browserGreeting() {
@@ -24,9 +46,6 @@ function browserGreeting() {
 }
 
 function tabListener() {
-	var tabMap = document.getElementById("tabMap");
-	var tabDirections = document.getElementById("tabDirections");
-
 	tabMap.onclick = tabMapSwitcher;
 	tabDirections.onclick = tabDirectionsSwitcher;
 }
@@ -37,6 +56,7 @@ function tabMapSwitcher() {
 	if (tabMapClass === "inactive") {
 		tabMap.getAttributeNode("class").value = "active";
 		tabDirections.getAttributeNode("class").value = "inactive";
+		formMap.style.zIndex = 115;
 	}
 }
 
@@ -46,10 +66,49 @@ function tabDirectionsSwitcher() {
 	if (tabDirClass === "inactive") {
 		tabDirections.getAttributeNode("class").value = "active";
 		tabMap.getAttributeNode("class").value = "inactive";
+		formMap.style.zIndex = 105;
 	}
 }
 
+function mainButtonListener() {
+	var dirMainButton = document.getElementById("dirMainButton");
+	var mapMainButton = document.getElementById("mapMainButton");
+	
+	dirMainButton.onclick = directionsPopupHandler;
+	mapMainButton.onclick = mapPopupHandler;
+}
 
+function directionsPopupHandler() {
+	popUpBackground.getAttributeNode("class").value = "show";
+	searchingPopup.getAttributeNode("class").value = "show";
+	setTimeout(function() {
+		searchingPopup.getAttributeNode("class").value = "hide";
+		directionsPopup.getAttributeNode("class").value = "show";
+	}, 2000);
+}
+
+function mapPopupHandler() {
+	popUpBackground.getAttributeNode("class").value = "show";
+	searchingPopup.getAttributeNode("class").value = "show";
+	setTimeout(function() {
+		searchingPopup.getAttributeNode("class").value = "hide";
+		mapPopup.getAttributeNode("class").value = "show";
+	}, 2000);
+}
+
+function popupCloseListener() {
+	var popupCloseIcon = document.getElementsByClassName("popupCloseIcon");
+
+	for (var i = 0; i < popupCloseIcon.length; i++) {
+		popupCloseIcon[i].onclick = popupCloseHandler;
+	}
+}
+// Closes all popups when clicking close. Add each new popup to this
+function popupCloseHandler() {
+	popUpBackground.getAttributeNode("class").value = "hide";
+	directionsPopup.getAttributeNode("class").value = "hide";
+	mapPopup.getAttributeNode("class").value = "hide";
+}
 
 
 
