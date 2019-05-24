ad858dc80b44c66deb4f0af08509daf1e71ab15b
John Holler <jh43051@gmail.com>
2019-05-24 14:33:00 -0400

    testing geocode callback

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ window.onload = function() {
 
 var tabMap = document.getElementById("tabMap");
 var tabDirections = document.getElementById("tabDirections");
+var dirMainButton = document.getElementById("dirMainButton");
+var mapMainButton = document.getElementById("mapMainButton");
 var formMap = document.getElementById("formMap");
 var popUpBackground = document.getElementById("popUpBackground");
 var searchingPopup = document.getElementById("searchingPopup");
@@ -83,9 +85,6 @@ function tabDirectionsSwitcher() {
 }
 
 function mainButtonListener() {
-	var dirMainButton = document.getElementById("dirMainButton");
-	var mapMainButton = document.getElementById("mapMainButton");
-	
 	dirMainButton.onclick = directionsPopupHandler;
 	mapMainButton.onclick = mapPopupHandler;
 }
@@ -106,6 +105,7 @@ function mapPopupHandler() {
 		searchingPopup.getAttributeNode("class").value = "hide";
 		mapPopup.getAttributeNode("class").value = "show";
 	}, 2000);
+	findMap();
 }
 
 function linksPopupListener() {
@@ -227,6 +227,76 @@ function popupCloseHandler() {
 	requestPopup.getAttributeNode("class").value = "linksPopups hide";
 }
 
+////////////////////////////////////////////////////////////////////////
+// Mapping Functionality
+////////////////////////////////////////////////////////////////////////
 
+// Authenticates communication with Here.com backend services
+var platform = new H.service.Platform({
+  'app_id': 'EtNIgjLba6MC6edi57vR',
+  'app_code': 'powLhtVer-MQAOPqWwgwsA'
+});
+
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+  document.getElementById("map"),
+  defaultLayers.normal.map,
+  {
+    zoom: 10,
+    center: { lat: 52.5, lng: 13.4 } // Berlin (change me)
+  }
+);
+
+// Create the default UI:
+var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+// Enable the event system on the map instance:
+var mapEvents = new H.mapevents.MapEvents(map);
+
+// Map event listener:
+map.addEventListener('tap', function(evt) {
+  // Log 'tap' and 'mouse' events:
+  console.log(evt.type, evt.currentPointer.type); 
+});
+
+// Instantiate the default behavior, providing the mapEvents object:
+var behavior = new H.mapevents.Behavior(mapEvents);
+
+
+function findMap() {
+	// Create the parameters for the geocoding request:
+	var geocodingParams = {
+			searchText: '200 S Mathilda Ave, Sunnyvale, CA'
+	};
+
+	// Define a callback function to process the geocoding response:
+	var onResult = function(result) {
+		var locations = result.Response.View[0].Result,
+			position,
+			marker;
+		// Add a marker for each location found
+		for (i = 0;  i < locations.length; i++) {
+		position = {
+			lat: locations[i].Location.DisplayPosition.Latitude,
+			lng: locations[i].Location.DisplayPosition.Longitude
+		};
+		marker = new H.map.Marker(position);
+		map.addObject(marker);
+		}
+	};
+
+	// Get an instance of the geocoding service:
+	var geocoder = platform.getGeocodingService();
+
+	// Call the geocode method with the geocoding parameters,
+	// the callback and an error callback function (called if a
+	// communication error occurs):
+	geocoder.geocode(geocodingParams, onResult, function(e) {
+		alert(e);
+	});
+	}
 
 }; // window.onload
